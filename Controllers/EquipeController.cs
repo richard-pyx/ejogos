92ea8190a7ff437a772b699a5d13855232067345
richard-pyx <richardsoooouza@gmail.com>
2022-05-10 19:57:16 -0400

    Tabela de equipes

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -1,4 +1,5 @@
 ﻿using EJOGOS.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EJOGOS.Controllers
@@ -19,6 +20,27 @@ namespace EJOGOS.Controllers
             ViewBag.Equipes = equipeModel.LerEquipes();
             
             return View();
+        }
+
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+            //criar um objeto equipe a partir do frontend.
+            //IFormColletion
+
+            Equipe novaEquipe = new Equipe();
+
+            novaEquipe.idEquipe = int.Parse(form["IdEquipe"]);
+            novaEquipe.Nome = form["Nome"];
+            novaEquipe.Imagem = form["Imagem"];
+
+            //chamar a função "Criar" passando um objeto do tipo equipe "novaEquipe".
+            equipeModel.Criar(novaEquipe);
+
+            ViewBag.Equipes = equipeModel.LerEquipes();
+            
+
+
+            return LocalRedirect("~/Equipe");
         }
     }
 }
